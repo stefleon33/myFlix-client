8b6a1e668722667673d2f46535cbb96ec58efc55
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-05-23 13:24:09 -0400

    Updated with movie variables

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -9,8 +9,28 @@ export const MovieView = ({ movie, onBackClick }) => {
           <span>{movie.title}</span>
         </div>
         <div>
+          <span>Description: </span>
+          <span>{movie.descritpion}</span>
+        </div>
+        <div>
+          <span>Description: </span>
+          <span>{movie.genre.descritpion}</span>
+        </div>
+        <div>
+          <span>Genre: </span>
+          <span>{movie.genre.Name}</span>
+        </div>
+        <div>
+          <span>Genre: </span>
+          <span>{movie.genre.descritpion}</span>
+        </div>
+        <div>
           <span>Director: </span>
           <span>{movie.director}</span>
+        </div>
+        <div>
+          <span>Bio: </span>
+          <span>{movie.director.bio}</span>
         </div>
         <button onClick={onBackClick}>Back</button>
       </div>
