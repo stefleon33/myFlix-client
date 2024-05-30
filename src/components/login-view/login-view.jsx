03b5f8721e8ed00938e0b562e852e552534f70fe
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-05-30 08:52:32 -0400

    Set value and onChange for username and password

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,15 +1,42 @@
 export const LoginView = () => {
-    return (
-      <form>
-        <label>
-          Username:
-          <input type="text" />
-        </label>
-        <label>
-          Password:
-          <input type="password" />
-        </label>
-        <button type="submit">Submit</button>
-      </form>
-    );
+  const [username, setUsername] = useState("");
+  const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+    event.preventDefault();
+
+    const data = {
+      access: username,
+      secret: password
+    };
+
+
+    fetch("https://movie-api33-c32ceac54882.herokuapp.com/users", {
+      method: "POST",
+      body: JSON.stringify(data)
+    });
   };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <input 
+            type="password" 
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+        />
+      </label>
+      <button type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
