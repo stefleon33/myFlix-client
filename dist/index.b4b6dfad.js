c42f9699fea4d803aebe19f8541b3de865197e69
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-06-07 20:53:43 -0400

    Wrapped the button in a link

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27578,9 +27578,16 @@ const MovieCard = ({ movie })=>{
                         lineNumber: 20,
                         columnNumber: 11
                     }, undefined),
-                    /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Button), {
-                        variant: "link",
-                        children: "Open"
+                    /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactRouterDom.Link), {
+                        to: `/movies/${encodeURIComponent(movie.id)}`,
+                        children: /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _reactBootstrap.Button), {
+                            variant: "link",
+                            children: "Open"
+                        }, void 0, false, {
+                            fileName: "src/components/movie-card/movie-card.jsx",
+                            lineNumber: 22,
+                            columnNumber: 13
+                        }, undefined)
                     }, void 0, false, {
                         fileName: "src/components/movie-card/movie-card.jsx",
                         lineNumber: 21,
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -18,9 +18,11 @@ export const MovieCard = ({ movie }) => {
         <Card.Body>
           <Card.Title>{movie.title}</Card.Title>
           <Card.Text>{movie.description}</Card.Text>
-          <Button variant="link">
-            Open
-          </Button>
+          <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+            <Button variant="link">
+              Open
+            </Button>
+          </Link>
         </Card.Body>
       </Card>
     );
