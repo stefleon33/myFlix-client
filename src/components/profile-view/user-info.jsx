1ed78b8c13adebf081093a44c112ce48f55b2416
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-06-10 15:43:09 -0400

    Added heading to user info component

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function UserInfo({ email, name}) {
     return (
         <>
+            <h4>Your Info</h4>
             <p>User: {name}</p>
             <p>Email: {email}</p>
         </>
