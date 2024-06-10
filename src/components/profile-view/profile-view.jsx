572c7ef5960a38cace7bd77d274c122a11e09901
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-06-10 15:26:34 -0400

    Returned new components

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -2,3 +2,11 @@ import "./profile-view.scss";
 import UserInfo from './user-info';
 import FavoriteMovies from './favorite-movies';
 import UpdateUser from './update-user';
+return (
+    <div>
+        <UserInfo  name={ user.Username} email={user.Email}/>
+        <FavoriteMovies favoriteMovieList={favoriteMovieList}/>
+        <UpdateUser handleSubmit={ handleSubmit} handleUpdate={handleUpdate}/>
+    </div>
+);
+}
