b007e85cedd7fa290c8a19d6ac26e66aa9a97e60
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-06-11 14:57:46 -0400

    Updated to include Form

--- src/components/profile-view/update-user.jsx
+++ src/components/profile-view/update-user.jsx
@@ -4,29 +4,66 @@ import Form from "react-bootstrap/Form";
 
 export const UpdateUser =({ formData, handleSubmit, handleUpdate, handleDeleteAccount }) =>  {
     return (
-        <form className='profile-form' onSubmit={(e) => handleSubmit(e)}>
-            <h2>Want to change some info?</h2>
-            <label>Username:</label>
-            <input
-                type='text'
-                name='Username'
-                defaultValue={user.Username}
-                onChange={e => handleUpdate(e)}/>
-            <label>Password</label>
-            <input
-                type='password'
-                name='password'
-                defaultValue={user.Password}
-                onChange={e => handleUpdate(e)}/>
-            <label>Email</label>
-            <input
-                type='email'
-                name='email'
-                defaultValue={user.Email}
-                onChange={e => handleUpdate(e)}/>
-            <button varitant='primary' type='sumbit'>
-                Update
-            </button>
-        </form>
+        <>
+            <h4>Update</h4>
+            <Form onSubmit={handleSubmit}>
+            <Form.Group>
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
+                type="text"
+                value={formData.username}
+                onChange={(e) => handleUpdate(e)}
+                required
+                minLength="3" 
+                placeholder="Enter a username"
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Password:</Form.Label>
+                <Form.Control
+                type="password"
+                value={formData.password}
+                onChange={(e) => handleUpdate(e)}
+                required
+                placeholder="Your password must be 8 or more characters"
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Email:</Form.Label>
+                <Form.Control
+                type="email"
+                value={formData.email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                placeholder="Enter your email address"
+                />
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label>Birthday:</Form.Label>
+                <Form.Control
+                type="date"
+                value={formData.birthday}
+                onChange={(e) => setBirthday(e.target.value)}
+                required
+                placeholder="Enter your birthday"
+                />
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit Changes
+            </Button>
+
+            <Button 
+                onClick={() => handleDeleteAccount()}
+                variant="outline-secondary"
+                className="mx-3"
+                >
+                Delete Account
+            </Button>
+            </Form>
+        </>           
     )
 }
