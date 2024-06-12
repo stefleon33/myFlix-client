64b98ccb05826aa4055fba41fd995ac0cb63793e
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-06-11 22:20:07 -0400

    Added styling/structure to MovieView

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import "./movie-view.scss";
+import { Button } from 'react-bootstrap';
+
 
 export const MovieView = ({ movies }) => {
     const {movieId} = useParams();
@@ -10,52 +12,58 @@ export const MovieView = ({ movies }) => {
     return (
       <div>
         <div>
-          <img  src={movie.image}/>
+          <img src={movie.image} className="img"/>
         </div>
-        <div>
-          <span>Title: </span>
-          <span>{movie.title}</span>
+        <div className="card-body bg-transparent"> 
+          <div className="fw-bold card text-center bg-transparent border-transparent">{movie.title}</div>
+          <ul className="list-group list-group-flush ">
+        <li className="list-group-item bg-transparent">
+          <span className="card-text fw-bold">Description: </span>
+          <span >{movie.description}</span></li>
+        </ul>                                       
         </div>
-        <div>
-          <span>Description: </span>
-          <span>{movie.description}</span>
-        </div>
-        <div>
-          <span>Genre: </span>
-          <span>{movie.genre.name}</span>
-        </div>
-        <div>
-          <span>Genre Description: </span>
-          <span>{movie.genre.description}</span>
-        </div>                                                                                              
-        <div>
-          <span>Director: </span>
-          <span>{movie.director.name}</span>
-        </div>
-        <div>
-          <span>Bio: </span>
-          <span>{movie.director.bio}</span>
-        </div>
-        <div>
-          <span>Birth: </span>
-          <span>{movie.director.birth}</span>
-        </div>
-        <div>
-          <span>Death: </span>
-          <span>{movie.director.death}</span>
-        </div>
-        <div>
-          <span>ID: </span>
-          <span>{movie.id}</span>
-        </div>
+        <ul className="list-group list-group-flush ">
+        <li className="list-group-item bg-transparent">
+          <div>
+            <span className="fw-bold">Genre: </span>
+            <span>{movie.genre.name}</span>
+          </div> 
+          <span className="fw-bold">Genre Description: </span>
+          <span>{movie.genre.description}</span></li>
+        </ul>                                                                                              
+        <ul className="list-group list-group-flush">
+        <li className="list-group-item bg-transparent">
+          <div>
+            <span className="fw-bold">Director: </span>
+            <span>{movie.director.name}</span>
+          </div>
+          <div>
+            <span className="fw-bold">Bio: </span>
+            <span>{movie.director.bio}</span>
+          </div>
+          <div> 
+            <span className="fw-bold">Birth: </span>
+            <span>{movie.director.birth}</span>
+          </div>
+          <div>
+             <span className="fw-bold">Death: </span>
+              <span>{movie.director.death}</span>
+          </div>
+          </li>
+          </ul>
+         <div style= {{display: "flex", justifyContent: "center"}}>
         <Link to= {`/`}>
-          <button
+          <Button
             className="back-button"
-            style={{ cursor: "pointer" }}
-          >
+            style={{ cursor: "pointer"}}>
             Back
-          </button>
+          </Button>
         </Link>
+        </div>
       </div>
     );
   };
+
+
+
+  
