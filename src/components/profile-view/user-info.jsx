7da2009c407844cbf703a2724f8f603bebd2eb00
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-06-13 22:12:08 -0400

    Changed heading

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -5,7 +5,7 @@ import PropTypes from "prop-types";
 export const UserInfo = ({ email, name, birthday}) => {
     return (
         <>
-            <h4>Your Info</h4>
+            <h4>My Profile</h4>
             <p>Name: {name}</p>
             <p>Email: {email}</p>
             <p>Birthday: {birthday}</p>
