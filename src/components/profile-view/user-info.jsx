b98aece158c949aab4236e3dbe3260b43bcee861
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-06-17 15:29:39 -0400

    Removed birthday from UserInfo

--- src/components/profile-view/user-info.jsx
+++ src/components/profile-view/user-info.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from "prop-types";
 
 
-export const UserInfo = ({ email, name, birthday}) => {
+export const UserInfo = ({ email, name }) => {
     return (
         <>
             <h4>My Profile</h4>
