abb53d5a9f8fe0a17538de7ff0739949aa7969d9
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-07-01 12:14:10 -0400

    Updated user reducer to include setToken and clearAuth

--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,14 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState ={
+    user: null,
+    token: null
+}
+
 const userSlice = createSlice({
     name: "user",
-    initialState: null,
+    initialState,
     reducers: {
         setUser: (state, action) => {
-            state.user = action.payload
+            state.user = action.payload;
+        },
+        setToken: (state, action) =>{
+            state.token = action.payload;
+        },
+        clearAuth: (state) => {
+            state.user = null;
+            state.token = null;
         }
     }
 });
 
-export const { setUser } = userSlice.actions;
+export const { setUser, setToken, clearAuth } = userSlice.actions;
 export default userSlice.reducer;
