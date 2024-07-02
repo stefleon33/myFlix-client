294053fc8a2f3253aab9adbb98e35a96a4d9f76e
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-07-02 11:56:51 -0400

    Added list to movies selector

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -8,7 +8,7 @@ import { useSelector } from "react-redux";
 
 export const MovieView = () => {
     const { movieId } = useParams();
-    const movies = useSelector((state) => state.movies);
+    const movies = useSelector((state) => state.movies.list);
 
     const movie = movies.find((b) => b.id === movieId);
 
