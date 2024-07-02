2bff6c00b07c660eb1695021714e73dcd874d7b3
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-07-02 11:58:54 -0400

    removed useState

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
