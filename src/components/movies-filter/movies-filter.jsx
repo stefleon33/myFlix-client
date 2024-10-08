74bbed709c7bec17c4a42796a8bd7d406d4537be
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-08 16:18:46 -0400

    search bar css added

--- src/components/movies-filter/movies-filter.jsx
+++ src/components/movies-filter/movies-filter.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import Form from "react-bootstrap/Form";
 import { setFilter } from "../../redux/reducers/movies";
+import "./movies-filter.scss";
+
 
 export const MoviesFilter = () => {
     const filter = useSelector((state) => state.movies.filter);
@@ -9,6 +11,7 @@ export const MoviesFilter = () => {
 
     return (
         <Form.Control
+        className="search-bar"
         type="text"
         placeholder="Search..."
         value={filter}
