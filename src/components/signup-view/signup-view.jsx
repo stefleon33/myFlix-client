4d6e4aeb9c3a36539bea6329c696b5c18e94b104
stefleon33 <158505324+stefleon33@users.noreply.github.com>
2024-10-09 11:17:17 -0400

    Scss for sign up view submit button

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import "./signup-view.scss";
 
 export const SignupView = () => {
   const [username, setUsername] = useState("");
@@ -74,7 +75,9 @@ export const SignupView = () => {
           required
         />
       </Form.Group>
-      <Button type="submit">
+      <Button 
+      className="submit-button"
+      type="submit">
         Submit
       </Button>
     </Form>
